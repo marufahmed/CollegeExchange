0659f86f24315ac276627e161140731f2c5bdbe0
Maruf Ahmed <marufahmed@u.boisestate.edu>
2018-12-05 16:48:25 -0700

    Fix the footer style.

--- login.php
+++ login.php
@@ -84,8 +84,8 @@ session_start();
     </div>
 </div>
 <footer>
-    <h2>About college exchange</h2>
-    <p>This website is intended for college students to trade in their items.</p>
+    <h2 class="custom-footer-heading">About college exchange</h2>
+    <p class="custom-footer-paragraph">This website is intended for college students to trade in their items.</p>
 </footer>
 
 <?php
