84b6326fe40b27f87e057fa48d19c4399f7a8290
Maruf Ahmed <marufahmed@u.boisestate.edu>
2018-12-05 16:39:49 -0700

    Fix the footer style.

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <footer>
-    <h2>About college exchange</h2>
-    <p>This website is intended for college students to trade in their items.</p>
+    <h2 class="custom-footer-heading">About college exchange</h2>
+    <p class="custom-footer-paragraph">This website is intended for college students to trade in their items.</p>
 </footer>
 </body>
 </html>
